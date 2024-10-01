1b87183dbf92d148d5df1d9a430e55bb1c31787a
Gabriel Gomes <106356258+gabrielbonfim2@users.noreply.github.com>
2024-10-01 11:58:09 -0300

    Update Questao.java

--- src/Questao.java
+++ src/Questao.java
@@ -101,9 +101,107 @@ public class Questao {
         questoes[4].opcaoE = "Neurulação";
         questoes[4].correta = "D";
 
-        questoes[5] = new Questao();
-        questoes[5].pergunta = "5.              "
-        questoes[5]
-        questoes[5]
-        questoes[5].pergunta = desk.
-        questoes[5]
+         questoes[5] = new Questao();
+        questoes[5].pergunta = "6. O que é o ecossistema?";
+        questoes[5].opcaoA = "Conjunto de todos os seres vivos de uma região";
+        questoes[5].opcaoB = "Relação entre predadores e presas";
+        questoes[5].opcaoC = "Conjunto de organismos e o meio ambiente interagindo";
+        questoes[5].opcaoD = "Clima de uma determinada área";
+        questoes[5].opcaoE = "Cadeia alimentar de uma região específica";
+        questoes[5].correta = "C";
+
+        questoes[6] = new Questao();
+        questoes[6].pergunta = "7. Qual é a unidade fundamental da vida?";
+        questoes[6].opcaoA = "Mitocôndria";
+        questoes[6].opcaoB = "Núcleo";
+        questoes[6].opcaoC = "Célula";
+        questoes[6].opcaoD = "Tecidos";
+        questoes[6].opcaoE = "Átomos";
+        questoes[6].correta = "C";
+
+        questoes[7] = new Questao();
+        questoes[7].pergunta = "8. Qual é a função dos leucócitos?";
+        questoes[7].opcaoA = "Transportar oxigênio";
+        questoes[7].opcaoB = "Regular o metabolismo";
+        questoes[7].opcaoC = "Atacar e destruir patógenos";
+        questoes[7].opcaoD = "Produzir anticorpos";
+        questoes[7].opcaoE = "Coagulação do sangue";
+        questoes[7].correta = "C";
+
+        questoes[8] = new Questao();
+        questoes[8].pergunta = "9. Qual é o nome do processo em que as plantas perdem água?";
+        questoes[8].opcaoA = "Osmose";
+        questoes[8].opcaoB = "Evaporação";
+        questoes[8].opcaoC = "Fotossíntese";
+        questoes[8].opcaoD = "Transpiração";
+        questoes[8].opcaoE = "Respiração";
+        questoes[8].correta = "D";
+
+        questoes[9] = new Questao();
+        questoes[9].pergunta = "10. Qual parte da célula é responsável pela produção de energia?";
+        questoes[9].opcaoA = "Núcleo";
+        questoes[9].opcaoB = "Ribossomos";
+        questoes[9].opcaoC = "Lisossomos";
+        questoes[9].opcaoD = "Mitocôndrias";
+        questoes[9].opcaoE = "Retículo endoplasmático";
+        questoes[9].correta = "D";
+
+        questoes[10] = new Questao();
+        questoes[10].pergunta = "11. Qual é a molécula responsável pelo armazenamento de informações genéticas?";
+        questoes[10].opcaoA = "RNA";
+        questoes[10].opcaoB = "Proteínas";
+        questoes[10].opcaoC = "Carboidratos";
+        questoes[10].opcaoD = "Lipídios";
+        questoes[10].opcaoE = "DNA";
+        questoes[10].correta = "E";
+
+        questoes[11] = new Questao();
+        questoes[11].pergunta = "12. Qual é o órgão responsável pela filtragem do sangue no corpo humano?";
+        questoes[11].opcaoA = "Coração";
+        questoes[11].opcaoB = "Fígado";
+        questoes[11].opcaoC = "Pulmões";
+        questoes[11].opcaoD = "Rins";
+        questoes[11].opcaoE = "Intestinos";
+        questoes[11].correta = "D";
+
+        questoes[12] = new Questao();
+        questoes[12].pergunta = "13. O que é a homeostase?";
+        questoes[12].opcaoA = "Processo de digestão";
+        questoes[12].opcaoB = "Regulação do ambiente interno do organismo";
+        questoes[12].opcaoC = "Reprodução celular";
+        questoes[12].opcaoD = "Transporte de nutrientes";
+        questoes[12].opcaoE = "Resposta a estímulos externos";
+        questoes[12].correta = "B";
+
+        questoes[13] = new Questao();
+        questoes[13].pergunta = "14. O que são os hormônios?";
+        questoes[13].opcaoA = "Moléculas de DNA";
+        questoes[13].opcaoB = "Moléculas de RNA";
+        questoes[13].opcaoC = "Mensageiros químicos no corpo";
+        questoes[13].opcaoD = "Estruturas celulares";
+        questoes[13].opcaoE = "Nutrientes essenciais";
+        questoes[13].correta = "C";
+
+        questoes[14] = new Questao();
+        questoes[14].pergunta = "15. Qual é a função dos cloroplastos nas células vegetais?";
+        questoes[14].opcaoA = "Respiração celular";
+        questoes[14].opcaoB = "Produção de energia";
+        questoes[14].opcaoC = "Fotossíntese";
+        questoes[14].opcaoD = "Armazenamento de água";
+        questoes[14].opcaoE = "Síntese de proteínas";
+        questoes[14].correta = "C";
+
+
+        for (Questao questao : questoes) {
+            questao.escrevaQuestao();
+            String resposta = questao.leiaResposta();
+            if (questao.isCorreta(resposta)) {
+                score++;
+            }
+        }
+
+
+        System.out.println("Você acertou " + score + " de " + questoes.length + " questões.");
+        scanner.close();
+    }
+}
