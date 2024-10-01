0b285a4ecae2e861b044fb52ae8c5941b8700734
Gabriel Gomes <106356258+gabrielbonfim2@users.noreply.github.com>
2024-10-01 12:13:04 -0300

    Update Questao.java

--- src/Questao.java
+++ src/Questao.java
@@ -11,7 +11,6 @@ public class Questao {
     String correta;
 
 
-System.out.println("Unifan - Centro Universitário Alfredo Nasser, nome do aluno :Gabriel Gomes de Oliveira Bonfim, professor: Brenno Pimenta. Seja bem-vindo, você responderá 15 perguntas envolvendo biologia"); 
 
 
     public boolean isCorreta(String resposta) {
